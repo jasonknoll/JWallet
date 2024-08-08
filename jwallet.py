979ceedd9e7ff94eef1630db5c2c3d9c3b773ef5
Jason Knoll <jasonknoll98@aol.com>
2024-08-07 22:11:43 -0400

    added basic ETH wallet generation

--- jwallet.py
+++ jwallet.py
@@ -1,4 +1,4 @@
-# TODO import dependencies for bitcoin, ethereum, and QR codes
+# TODO Support BTC, ETH, ETC, and USDT
 # TODO upon generating an address, check with a blockchain API to see if it already exists
 # TODO setup argparse to handle CLI arguments
 
@@ -9,9 +9,26 @@ import bitcoinlib
 import eth_account
 import requests
 import qrcode
+import os
 
 
 def generate_qr_code(data):
     qr = qrcode.make(data)
 
     # qr.save("wallet.png")
+
+
+def generate_eth_account():
+    # Create a private key and address combo, with a random number for extra randomness
+    wallet = eth_account.Account.create(os.urandom(32))
+
+    # print(f"Key: {wallet.key.hex()}\nAddress: {wallet.address}")
+    # TODO display address and key in secure way, with warning before exit
+    # NOTE make sure to check that wallet doesn't exist and that it doesn't
+    # save to bash history
+
+
+def generate_btc_account():
+    pass
+
+
